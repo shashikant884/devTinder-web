a602da5561ab727063a68b557f069f4b7c9558ba
shashikant884 <yadavshashikant884@gmail.com>
2025-01-13 13:17:09 +0530

    Redirect to login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Body from "./Body";
+import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import { Provider } from "react-redux";
---
+++ src/components/Body.jsx
@@ -0,0 +1,43 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
+
+const Body = () => {
+  const dispach = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+  const fatchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispach(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!userData) {
+      fatchUser();
+    }
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
