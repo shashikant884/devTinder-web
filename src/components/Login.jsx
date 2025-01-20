e555efea9c5befbd8f5df45a863e3bf1b7dd6153
shashikant884 <yadavshashikant884@gmail.com>
2025-01-20 16:58:12 +0530

    SignUp

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,12 +6,35 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("shashi@gmail.com");
-  const [password, setPassword] = useState("Shashi@123");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isLoginFrom, SetIsLoginFrom] = useState(false);
   const dispach = useDispatch();
   const navigate = useNavigate();
   const [error, setError] = useState("");
 
+  const handleSignUp = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        {
+          firstName,
+          lastName,
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      console.log(res);
+
+      dispach(addUser(res.data.data));
+      return navigate("/profile");
+    } catch (err) {
+      setError(err?.response?.data || "Something went wrong");
+    }
+  };
   const handLogin = async () => {
     try {
       const res = await axios.post(
@@ -25,7 +48,7 @@ const Login = () => {
       dispach(addUser(res.data));
       return navigate("/");
     } catch (err) {
-      setError(err?.response?.data);
+      setError(err?.response?.data || "Something went wrong");
     }
   };
 
@@ -33,8 +56,38 @@ const Login = () => {
     <div className="flex justify-center my-10">
       <div className="card bg-base-300 w-96 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title justify-center">Login</h2>
+          <h2 className="card-title justify-center">
+            {isLoginFrom ? "Login" : "Sign Up"}
+          </h2>
           <div>
+            {!isLoginFrom && (
+              <>
+                <label className="form-control w-full max-w-xs py-2">
+                  <div className="label">
+                    <span className="label-text">First Name </span>
+                  </div>
+                  <input
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
+                  />
+                </label>
+                <label className="form-control w-full max-w-xs py-2">
+                  <div className="label">
+                    <span className="label-text">Last Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
+                  />
+                </label>
+              </>
+            )}
             <label className="form-control w-full max-w-xs py-2">
               <div className="label">
                 <span className="label-text">Email ID</span>
@@ -62,10 +115,23 @@ const Login = () => {
           </div>
           <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center">
-            <button className="btn btn-primary" onClick={handLogin}>
-              Login
+            <button
+              className="btn btn-primary"
+              onClick={isLoginFrom ? handLogin : handleSignUp}
+            >
+              {isLoginFrom ? "Login" : "Sign Up"}
             </button>
           </div>
+          <p
+            className="m-auto"
+            onClick={() => {
+              SetIsLoginFrom((value) => !value);
+            }}
+          >
+            {isLoginFrom
+              ? "New User? signup here"
+              : "existing user? Login Here"}
+          </p>
         </div>
       </div>
     </div>
